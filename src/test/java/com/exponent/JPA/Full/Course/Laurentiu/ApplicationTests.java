ad62be1c49a5752a048541eedccfec5173d9ae45
Geoffrey Makawa <geoffreymakawa04@gmail.com>
2024-09-27 03:42:56 +0200

    Started JPA full course from scratch

---
+++ src/test/java/com/exponent/JPA/Full/Course/Laurentiu/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exponent.JPA.Full.Course.Laurentiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
