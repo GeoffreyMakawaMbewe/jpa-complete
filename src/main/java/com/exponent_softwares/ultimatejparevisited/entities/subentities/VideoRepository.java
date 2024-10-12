36af0bcf40c9e33d1a7045d81b2af815aca70ad7
Geoffrey Makawa <geoffreymakawa04@gmail.com>
2024-10-12 23:29:54 +0200

    Removed from master branch to last section

---
+++ src/main/java/com/exponent_softwares/ultimatejparevisited/entities/subentities/VideoRepository.java
@@ -0,0 +1,6 @@
+package com.exponent_softwares.ultimatejparevisited.entities.subentities;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VideoRepository extends JpaRepository<Video, Integer> {
+}
