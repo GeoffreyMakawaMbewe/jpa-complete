3f5e3d555bf693a0023684f979c7a6791939b776
Geoffrey Makawa <geoffreymakawa04@gmail.com>
2024-10-12 14:32:11 +0200

    Relationships implemented in this branch

---
+++ src/main/java/com/exponent_softwares/ultimatejparevisited/entities/Resource.java
@@ -0,0 +1,32 @@
+package com.exponent_softwares.ultimatejparevisited.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Resource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE,
+            generator = "resource_table"
+    )
+    @TableGenerator(
+            name = "resource_table",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
+            initialValue = 1,
+            allocationSize = 1
+    )
+    private Integer id;
+    private String name;
+    private String url;
+
+    @OneToOne
+    @JoinColumn(name = "lecturer-id") //BiDirectional relationships makes it difficult to maintain all the foreign keys
+    private Lecturer lecturer;
+}
