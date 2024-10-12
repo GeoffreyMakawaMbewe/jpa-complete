5590d7648dc2a6b6445e9e0dffc78abb890c07d6
Geoffrey Makawa <geoffreymakawa04@gmail.com>
2024-10-12 23:32:59 +0200

    cleaned master branch

--- src/main/java/com/exponent_softwares/ultimatejparevisited/entities/subentities/VideoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.exponent_softwares.ultimatejparevisited.entities.subentities;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoRepository extends JpaRepository<Video, Integer> {
-}
