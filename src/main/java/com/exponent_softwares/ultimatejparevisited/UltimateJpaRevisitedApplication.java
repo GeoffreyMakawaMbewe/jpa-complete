df3e62b9b5244f1db1f7c5c0bda47f54026f2a5c
Geoffrey Makawa <geoffreymakawa04@gmail.com>
2024-10-12 14:10:21 +0200

    Moved code from master branch

--- src/main/java/com/exponent_softwares/ultimatejparevisited/UltimateJpaRevisitedApplication.java
+++ src/main/java/com/exponent_softwares/ultimatejparevisited/UltimateJpaRevisitedApplication.java
@@ -1,16 +1,8 @@
 package com.exponent_softwares.ultimatejparevisited;
 
-import com.exponent_softwares.ultimatejparevisited.entities.Author;
-import com.exponent_softwares.ultimatejparevisited.repositories.AuthorRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class UltimateJpaRevisitedApplication {
@@ -19,58 +11,58 @@ public class UltimateJpaRevisitedApplication {
         SpringApplication.run(UltimateJpaRevisitedApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(AuthorRepository authorRepository){
-
-       return args -> {
-
-
-           Author author1 = Author
-                   .builder()
-                   .firstName("John")
-                   .lastName("Banda")
-                   .email("banda@gmail.com")
-                   .age(20)
-                   .build();
-           Author author2 = Author
-                   .builder()
-                   .firstName("Bill")
-                   .lastName("Banda")
-                   .email("bbanda@gmail.com")
-                   .age(21)
-                   .build();
-           Author author3 = Author
-                   .builder()
-                   .firstName("James")
-                   .lastName("Banda")
-                   .email("jbanda@gmail.com")
-                   .age(24)
-                   .build();
-           Author author4 = Author
-                   .builder()
-                   .firstName("Martha")
-                   .lastName("Banda")
-                   .email("mbanda@gmail.com")
-                   .age(22)
-                   .build();
-           Author author5 = Author
-                   .builder()
-                   .firstName("Osman")
-                   .lastName("Banda")
-                   .email("obanda@gmail.com")
-                   .age(25)
-                   .build();
-
-           List<Author> authorList = new ArrayList<>();
-           authorList.add(author1);
-           authorList.add(author2);
-           authorList.add(author3);
-           authorList.add(author4);
-           authorList.add(author5);
-
-           authorRepository.saveAll(authorList);
-
-       };
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(AuthorRepository authorRepository){
+//
+//       return args -> {
+//
+//
+//           Author author1 = Author
+//                   .builder()
+//                   .firstName("John")
+//                   .lastName("Banda")
+//                   .email("banda@gmail.com")
+//                   .age(20)
+//                   .build();
+//           Author author2 = Author
+//                   .builder()
+//                   .firstName("Bill")
+//                   .lastName("Banda")
+//                   .email("bbanda@gmail.com")
+//                   .age(21)
+//                   .build();
+//           Author author3 = Author
+//                   .builder()
+//                   .firstName("James")
+//                   .lastName("Banda")
+//                   .email("jbanda@gmail.com")
+//                   .age(24)
+//                   .build();
+//           Author author4 = Author
+//                   .builder()
+//                   .firstName("Martha")
+//                   .lastName("Banda")
+//                   .email("mbanda@gmail.com")
+//                   .age(22)
+//                   .build();
+//           Author author5 = Author
+//                   .builder()
+//                   .firstName("Osman")
+//                   .lastName("Banda")
+//                   .email("obanda@gmail.com")
+//                   .age(25)
+//                   .build();
+//
+//           List<Author> authorList = new ArrayList<>();
+//           authorList.add(author1);
+//           authorList.add(author2);
+//           authorList.add(author3);
+//           authorList.add(author4);
+//           authorList.add(author5);
+//
+//           authorRepository.saveAll(authorList);
+//
+//       };
+//    }
 
 }
