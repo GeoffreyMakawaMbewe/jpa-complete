f7bc7f99fa6ecb1fdeb3a9df59c038a7939fffb3
Geoffrey Makawa <geoffreymakawa04@gmail.com>
2024-10-12 23:29:54 +0200

    Removed from master branch to last section

---
+++ src/main/java/com/exponent_softwares/ultimatejparevisited/entities/Resource.java
@@ -0,0 +1,35 @@
+package com.exponent_softwares.ultimatejparevisited.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@Entity
+@SuperBuilder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+//@DiscriminatorColumn(name = "Discriminator", discriminatorType = DiscriminatorType.STRING, length = 10) //ONLY NEEDED FOR SINGLE TABLE STRATEGY
+public class Resource {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE,
+            generator = "resource_table"
+    )
+    @TableGenerator(
+            name = "resource_table",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
+            initialValue = 1,
+            allocationSize = 1
+    )
+    private Integer id;
+    private String name;
+    private int size;
+    private String url;
+
+    @OneToOne
+    @JoinColumn(name = "lecturer-id") //BiDirectional relationships makes it difficult to maintain all the foreign keys
+    private Lecturer lecturer;
+}
